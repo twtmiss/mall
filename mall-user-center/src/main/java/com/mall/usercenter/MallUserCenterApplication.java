b73abe36937c23075c257ac2304b360228d1a900
totalo <totalo@qq.com>
2020-08-25 00:29:08 +0800

    Initial commit

---
+++ mall-user-center/src/main/java/com/mall/usercenter/MallUserCenterApplication.java
@@ -0,0 +1,13 @@
+package com.mall.usercenter;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class MallUserCenterApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(MallUserCenterApplication.class, args);
+    }
+
+}
